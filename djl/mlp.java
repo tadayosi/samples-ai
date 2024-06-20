61c86ea69380f954c2291ae8d9fe3a3c8408ebda
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2024-06-21 00:16:30 +0900

    update djl mlp

--- djl/mlp.java
+++ djl/mlp.java
@@ -1,7 +1,8 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //DEPS ai.djl:bom:0.28.0@pom
 //DEPS ai.djl:api
-//DEPS ai.djl.mxnet:mxnet-model-zoo
+//DEPS ai.djl:model-zoo
+//DEPS ai.djl.pytorch:pytorch-engine
 //DEPS org.slf4j:slf4j-simple:1.7.36
 
 import static java.lang.System.*;
