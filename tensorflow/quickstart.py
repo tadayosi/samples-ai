947838d225f50ddc6964dee188362a58e59adc3b
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2024-12-11 22:26:58 +0900

    tf - add quickstart

---
+++ tensorflow/quickstart.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+
+print("TensorFlow version:", tf.__version__)
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.models.Sequential(
+    [
+        tf.keras.layers.Input(shape=(28, 28)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(10),
+    ]
+)
+print(model.summary())
+
+model.compile(
+    optimizer="adam",
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=["accuracy"],
+)
+
+model.fit(x_train, y_train, epochs=5)
+
+model.evaluate(x_test, y_test, verbose=2)
+
+model.export("build/tensorflow/mnist")
